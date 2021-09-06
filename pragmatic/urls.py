cddac3e26d745001e65a83ecede6979eba0ab55a
Jinah-engineer <jeeeengineer@gmail.com>
2021-09-06 15:27:49 +0900

    profileapp implementation

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -1,7 +1,8 @@
 # Routing - 특정 주소를 들어갔을 때, view 를 돌려줘야 한다
 # 특정 주소를 만들어주는 작업 = urls.py
 # 제일 먼저 요청을 받는 파일 !!!
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -11,6 +12,7 @@ def trigger_error(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sentry-debug/', trigger_error),
-    path('accounts/', include('accountapp.urls'))  # accountapps.urls.py 의 파일을 참조하여 안에서 다시 분기를 하는
-]
+    path('accounts/', include('accountapp.urls')),  # accountapps.urls.py 의 파일을 참조하여 안에서 다시 분기를 하는
+    path('profiles/', include('profileapp.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
