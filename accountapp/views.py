1e7c1d3ac927a00935f6f510abf6e7ebb26fb6ad
Jinah-engineer <jeeeengineer@gmail.com>
2021-11-20 16:34:59 +0900

    subscription app 생성

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,9 +10,11 @@ from django.shortcuts import render
 # Render response
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
+from articleapp.models import Article
 
 """
     Account
@@ -75,10 +77,16 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 # 회원정보
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/user_detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 # 계정정보 수정
 @method_decorator(has_ownership, 'get')
